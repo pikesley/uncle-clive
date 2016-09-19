752de83b2d011bdce7b0fb0d2e2cac3c57cd385a
pikesley <github@orgraphone.org>
2016-09-19 21:59:05 +0100

    v2

--- lib/uncle_clive.rb
+++ lib/uncle_clive.rb
@@ -27,6 +27,71 @@ module UncleClive
       end
     end
 
+    get '/font/:text' do
+      headers( 'Access-Control-Allow-Origin' => '*' )
+
+      text = cleave params[:text], separator: params.fetch('line-separator', '---')
+
+      respond_to do |wants|
+        wants.json do
+          Nineteen::Eighty::Two::Formats::JSON.format text
+        end
+
+        wants.svg do
+          Nineteen::Eighty::Two::Formats::SVG.format text, {colour: "##{params.fetch('colour', '000000')}"}
+        end
+
+        wants.text do
+          Nineteen::Eighty::Two::Formats::Text.format text, {on: '()', off: '  '}
+        end
+
+        wants.html do
+          @content = Nineteen::Eighty::Two::Formats::HTMLTable.format text
+          erb :table, layout: :default
+        end
+      end
+    end
+
+    get '/colours/:colour' do
+      headers 'Access-Control-Allow-Origin' => '*'
+
+      colour = params.fetch 'colour'
+      hex = Nineteen::Eighty::Two::Colours[colour]
+
+      respond_to do |wants|
+        wants.text do
+          hex
+        end
+
+        wants.other do
+          {
+            'id' => colour,
+            'hex' => hex
+          }.to_json
+        end
+      end
+    end
+
+    get '/messages/:key' do
+      headers 'Access-Control-Allow-Origin' => '*'
+
+      key = params.fetch 'key'
+      message = Nineteen::Eighty::Two::Messages[key]
+
+      respond_to do |wants|
+        wants.text do
+          message  
+        end
+
+        wants.other do
+          {
+            'id' => key,
+            'message' => message
+          }.to_json
+        end
+      end
+    end
+
     get '/:text' do
       headers( 'Access-Control-Allow-Origin' => '*' )
 
